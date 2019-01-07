9509f289bf5847df40299302f0de97af301f01a7
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-07 03:20:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@ require 'pry'
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0 
     "FizzBuzz"
+  binding.pry
   elsif int % 5 == 0 
     "Buzz"
   elsif int % 3 == 0 
     "Fizz"
   else
   end
-binding.pry
 end
